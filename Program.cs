9767f1fb52638bdabb05458e9764195271e20159
lodemisplon <65070003+lodemisplon@users.noreply.github.com>
2025-05-07 20:46:33 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World! Cloud Ev");
+app.MapGet("/", () => "Hello Lode's World! Cloud Ev");
 
 app.Run();
